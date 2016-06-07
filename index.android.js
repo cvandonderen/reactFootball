905621e07a18d038704619c63ba5b82ddd06c7e2
Casper van Donderen <casper@comoyo.com>
2016-06-07 21:03:36 +0200

    Add initial loading screen

--- index.android.js
+++ index.android.js
@@ -6,47 +6,56 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    ProgressBarAndroid,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
+import Config from './config'
 
-class reactFootball extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var reactFootball = React.createClass({
+    getInitialState: function() {
+        return {
+            isLoading:true
+        };
+    },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    fetchMatches: function() {
+        fetch("http://pads6.pa-sport.com/api/football/competitions/matchDay/" + Config.API_KEY + "/20160508/json", {method: "GET"})
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log(JSON.stringify(responseData.matches));
+        })
+        .catch((error) => console.log(error))
+        .done();
+    },
+
+    render: function() {
+        if (this.state.isLoading) {
+            return this.renderInitialLoad();
+        }
+
+        return (
+            <View>
+
+            </View>
+        );
+    },
+
+    renderInitialLoad: function() {
+        return (
+            <View>
+                <ProgressBarAndroid styleAttr="Normal" />
+            </View>
+        )
+    },
+
+    componentWillMount: function() {
+        this.fetchMatches();
+    }
 });
+
+var styles = StyleSheet.create({});
 
 AppRegistry.registerComponent('reactFootball', () => reactFootball);
