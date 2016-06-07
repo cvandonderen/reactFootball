ab6fd158cfcb19dc3aadb957ba01b39262a473e7
Casper van Donderen <casper@comoyo.com>
2016-06-07 22:09:22 +0200

    Add main page content

--- index.android.js
+++ index.android.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import {
     AppRegistry,
+    ListView,
     ProgressBarAndroid,
     StyleSheet,
     Text,
@@ -17,7 +18,10 @@ import Config from './config'
 var reactFootball = React.createClass({
     getInitialState: function() {
         return {
-            isLoading:true
+            dataSource:new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2
+            }),
+            isLoading: true,
         };
     },
 
@@ -25,7 +29,10 @@ var reactFootball = React.createClass({
         fetch("http://pads6.pa-sport.com/api/football/competitions/matchDay/" + Config.API_KEY + "/20160508/json", {method: "GET"})
         .then((response) => response.json())
         .then((responseData) => {
-            console.log(JSON.stringify(responseData.matches));
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(responseData.matches.match),
+                isLoading: false
+            });
         })
         .catch((error) => console.log(error))
         .done();
@@ -37,18 +44,40 @@ var reactFootball = React.createClass({
         }
 
         return (
-            <View>
-
-            </View>
+            <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderMatch} />
         );
     },
 
     renderInitialLoad: function() {
         return (
-            <View>
+            <View style={styles.loading}>
                 <ProgressBarAndroid styleAttr="Normal" />
             </View>
-        )
+        );
+    },
+
+    renderMatch: function(match) {
+        return (
+            <View style={styles.match}>
+                <Text style={styles.homeTeamName}>
+                    {match.homeTeam.teamName}
+                </Text>
+                <View style={styles.scores}>
+                    <Text style={styles.score}>
+                        {match.homeTeam.score}
+                    </Text>
+                    <Text>-</Text>
+                    <Text style={styles.score}>
+                        {match.awayTeam.score}
+                    </Text>
+                </View>
+                <Text style={styles.awayTeamName}>
+                    {match.awayTeam.teamName}
+                </Text>
+            </View>
+        );
     },
 
     componentWillMount: function() {
@@ -56,6 +85,41 @@ var reactFootball = React.createClass({
     }
 });
 
-var styles = StyleSheet.create({});
+var styles = StyleSheet.create({
+    awayTeamName: {
+        flex: 4,
+        textAlign: 'right'
+    },
+
+    homeTeamName: {
+        flex: 4
+    },
+
+    loading: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    match: {
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 10
+    },
+
+    score: {
+        fontSize: 18
+    },
+
+    scores: {
+        alignItems: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+
+});
 
 AppRegistry.registerComponent('reactFootball', () => reactFootball);
