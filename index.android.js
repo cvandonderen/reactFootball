a06d9fdba7d26932153d350365211f9519e3047b
Casper van Donderen <casper@comoyo.com>
2016-06-08 18:03:03 +0200

    Add Back button navigation

--- index.android.js
+++ index.android.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import {
     AppRegistry,
+    BackAndroid,
     Navigator
 } from 'react-native';
 import DetailPage from './detailPage'
 import MainPage from './mainPage'
 
+var _navigator;
+
 var reactFootball = React.createClass({
     navigatorRenderScene: function(route, navigator) {
+        _navigator = navigator;
         switch (route.id) {
             case 'main':
                 return (
@@ -28,7 +32,16 @@ var reactFootball = React.createClass({
                 initialRoute={{id: 'main'}}
                 renderScene={this.navigatorRenderScene}/>
         );
+    },
+
+});
+
+BackAndroid.addEventListener('hardwareBackPress', () => {
+    if (_navigator.getCurrentRoutes().length === 1  ) {
+        return false;
     }
+    _navigator.pop();
+    return true;
 });
 
 AppRegistry.registerComponent('reactFootball', () => reactFootball);
